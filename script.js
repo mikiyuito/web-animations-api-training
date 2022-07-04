98a00068d097be70ad96262e4172782540f9d0dc
ユイト <69744608+mikiyuito@users.noreply.github.com>
2022-07-04 18:17:54 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let roateColor = [
+  { transform: "roate(1turn)", backgroundColor: "#000" },
+  { backgroundColor: "#f90351", offset: 0.2 }
+];
+
+let roateColorTiming = {
+  duration: 3000,
+  iterations: Infinity
+};
+
+console.log(roateColor);
